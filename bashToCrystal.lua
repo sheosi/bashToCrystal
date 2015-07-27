5aec59bcba83ad2af35384713e6adca3c082d41a
Sheosi <sertorbe@gmail.com>
2015-07-27 19:39:33 +0200

    lua: finished parser parser, moved to a new file

--- bashToCrystal.lua
+++ bashToCrystal.lua
@@ -1,111 +1,26 @@
+require "patternLang"
 local lpeg = 	require "lpeg"
-local ins = require "inspect"
 
-function printContents( )
-	table = getmetatable(Cb("parameters"))
-
-	for key,value in pairs(table.__index) do --actualcode
-    	print(key,": ",value)
-	end
-
-end
-
---io.input("example.sh")
---textbuffer = io.read("*all")
---Lpeg set
-----------------------------
-P = lpeg.P
-V = lpeg.V
-R = lpeg.R
-C = lpeg.C
-Cg = lpeg.Cg
-Cb = lpeg.Cb
-Cp = lpeg.Cp
-Cc = lpeg.Cc
-Cf = lpeg.Cf
-Ct = lpeg.Ct
-
-locale ={}
-lpeg.locale(locale)   -- adds locale entries into 'lpeg' table
-
-space = locale.space
-character = locale.alpha
+import = parse("Import [import]")
+parameters = parse('Parameters "$@"[parameters]{Ct( (" "*C(string))^1 )}')
+newPattern("assignation", "[scope]{ <local (local)> + <(global)> }[left]=_$_'('_[right]{C(string^1)}_')'" )
 
 
-string = (character+'_')^1
-
---File definition
-------------------------------------
-function append( table, value)
-	table[#table]=value
-end
-function pair( name,pattern)
-	if type(pattern) == "string" then
-		return Cg(Cc(name)*Cc(pattern))
-	else
-		return Cg(Cc(name)*pattern)
-	end
-end
-
-
-function valueString( name )
-	return pair(name,C(string) )
-end
-function parse(str)
-	local pairPattern = Cf('['*Ct("")* pair("type","value")*valueString("pattern")*']',rawset)
-	local optSpace = P'_'*Cf(Ct("")*pair("type","optwhitespace"),rawset)
-	local other = Cf(Ct("")*pair("type","constantPattern")*pair("string",C(P'$'+P'=') )
-		
-	--local patternResult = pairPattern:match(str)
-	--if patternResult~=nil then
-		--return valueString(patternResult)
-	--elseif P'_':match(str) then
-		--return space^0
-	--else
-		--return P(str)
-	--end
-	local parsePattern= Ct(pairPattern + optSpace)
-	local result = parsePattern:match(str)
-	if result == nil then
-		return P(str)
-	end
-	if result[1].type == "value" then
-		return valueString(result[1].pattern)
-	elseif result[1].type == "optwhitespace" then
-		return space^0
-	end
-end
---pattern ("import","Import [import]")
-import = 'Import' * space^1 * valueString("import")
-parameters = 'Parameters' * space^1 * '"$@"' * pair("parameters",  Ct( (' ' * C(string) )^1 ))
-assignation = pair("type","assignation")* pair( "scope", ('local'* space^1)*Cc('local') + Cc('global') )
- * parse("[left]") * parse('=')
-  * parse('_') *
-parse('$')
-   *parse('_')*'('*space^0*pair("right",C(string^1))* space^0*')'
---pair("type","assignation")*pair( "scope",  ) * valueString("left") * '=' *space^0 * '$'*space^0*'('*space^0*pair("right",C(string^1))* space^0*')'
---"[scope]{'local' (local) |(global) }-$[left]_'='_"
---"Import [import]"
 
 endline =   P'\n'
 inst = assignation
-func = 'function'* space^1 *pair("type","function") * valueString("name",string) * space^0 * '()'* space^0 *'{' * space^0 * parameters^0 *
-Cg(Cc("instructions")*Ct(""))
-* space^0*'}'
+newPattern('func','function [name]_()_{_}')
+ 
 
-pattern = assignation
+pattern = func
 createTable=Cf(lpeg.Ct("")* pattern,rawset)
 
-textbuffer = [[function Get_Archive_Format ()     {
-	   Parameters "$@" archive
-}]]
-textbuffer = [[
-local format=$(Downcase  )]]
+textbuffer = [[function Get_Archive_Format ()  {}]]
 table = createTable:match(textbuffer)
 
 if table == nil then
 	table = {}
 end
 for key,value in pairs(table) do --actualcode
-   	print(key,": ",value)
+	print(key,": ",value)
 end
---
+++ patternLang.lua
@@ -0,0 +1,122 @@
+local lpeg = 	require "lpeg"
+
+
+
+--io.input("example.sh")
+--textbuffer = io.read("*all")
+--Lpeg set
+----------------------------
+P = lpeg.P
+V = lpeg.V
+R = lpeg.R
+C = lpeg.C
+Cg = lpeg.Cg
+Cb = lpeg.Cb
+Cp = lpeg.Cp
+Cc = lpeg.Cc
+Cf = lpeg.Cf
+Ct = lpeg.Ct
+
+locale ={}
+lpeg.locale(locale)   -- adds locale entries into 'lpeg' table
+
+space = locale.space
+character = locale.alpha
+anyChar = 1 
+
+string = (character+'_')^1
+
+--File definition
+------------------------------------
+function parseOperator( table )
+	if table.type == "value" then
+		if table.valpattype == "string" then
+			return valueString(table.name)
+		elseif table.valpattype == "lpegPattern" then
+			--Substitute "<" with "parse('" , and ">" with "')"
+			local subsPattern=lpeg.Cs( ( (P'<'/"parse('") + (P'>'/"')"+1))^0 )
+			table.pattern = subsPattern:match(table.pattern)
+
+			return pair( table.name,assert(loadstring("return "..table.pattern))())
+		end
+	elseif table.type == "optwhitespace" then
+		return space^0
+	elseif table.type == "mandatoryspace" then
+		return space^1
+	elseif table.type == "export" then
+		return Cc(table.string)
+	elseif table.type == "constantPattern" then
+		return P(table.string)
+	elseif table.type == "orPattern" then
+		return parseOperator(table.left)+parseOperator(table.right)
+	end
+end
+function pair( name,pattern)
+	if type(pattern) == "string" then
+		return Cg(Cc(name)*Cc(pattern))
+	else
+		return Cg(Cc(name)*pattern)
+	end
+end
+
+function ttype( string )
+	return pair("type",string)
+end
+function valueString( name )
+	return pair(name,C(string) )
+end
+function tablify( tabletype,pattern )
+	return Cf(Ct("")*ttype(tabletype)*pattern,rawset)
+end
+function printTable(self)
+	if self == nil then
+		print(nil)
+	else
+		for key,table in pairs(self)do
+			for key,value in pairs(table) do --actualcode
+				print(key,": ",value)
+			end
+		print("---------------")
+		end
+		print("================")
+	end
+end
+
+
+function newPattern( name,pattern )
+	if type(pattern) == "string" then
+		pattern=ttype(name)*parse(pattern)
+	else
+		pattern=ttype(name)*pattern
+	end
+	_G[name]=pattern
+end
+--Own syntax for parsing
+--[name]: defines a pair for the output table, expects a string, <name> will be the key of the pair
+--[name]{pattern}: same as above but expects <pattern> instead of a string
+--(name): when the pattern is true it will emit <name> as constant (just like lpeg.Cc)
+--_: 0 or more spaces
+-- : 1 or more spaces
+-- any other thing: will generate a pattern with that string, symbol, whatever
+--'char': when you need to use anything which is already in the syntax use single quotes. NOTE: single quotes only acceps one character
+function parse(str)
+	local luaChars = (character+space+"^"+'('+')'+"'"+locale.digit+'+'+'<'+'>'+'"'+'*')^1
+
+	local pairPattern = tablify("value",'[' *valueString("name")*']'*Cg(Cc("valpattype")*(P'{'* Cc"lpegPattern"+ Cc"string") )*pair("pattern",C(luaChars)*'}')^-1)
+	local optSpace    = tablify("optwhitespace",P'_')
+	local mandatorySpace = tablify("mandatoryspace",P' ')
+	local exportConstant = tablify("export",'('*valueString("string")*')')
+	local other       = tablify( "constantPattern",pair("string",C(P'$'+P'='+character^1+'"'+'@')+ P"'"*C(anyChar)*"'"  ) )
+	
+	local allPatterns = pairPattern + optSpace + mandatorySpace + other + exportConstant
+	local parsePattern= Ct( (allPatterns)^1 )
+	local orPattern = tablify("orPattern",pair("left",parsePattern)*'|'*pair("right",parsePattern))
+	parsePattern = Ct( (allPatterns+orPattern)^1 )
+
+	local patResult = parsePattern:match(str)
+	local parseResult = P''
+	for _,result in ipairs(patResult) do
+		parseResult= parseResult*parseOperator(result)
+	end
+	return parseResult
+end
