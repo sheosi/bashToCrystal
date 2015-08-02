70831f26b68401610eff28e24347beafc54e4344
Sheosi <sertorbe@gmail.com>
2015-08-02 18:18:44 +0200

    refactored switch.lua

--- switch.lua
+++ switch.lua
@@ -1,19 +1,19 @@
 function switch( input )
 	return function (caseTable)
 		local f
-		if (input) then
-				f = caseTable[input] or caseTable.default
-			else
-				f = caseTable.missing or caseTable.default
-			end
-			if f then
-				if type(f)=="function" then
-					return f(input,self)
-				else
-					return f
-				end
-			else
-				error("switch had an error")
-			end
+
+		if input then f = caseTable[input]  or caseTable.default
+		else		  f = caseTable.missing or caseTable.default --nil values
 		end
+
+		if f then 
+			if type(f)=="function" then return f(input,self)
+			else                        return f
+			end
+
+		else
+			error("switch:",input," has no entry and there's no 'default' one")
+			
+		end
+	end
 end
